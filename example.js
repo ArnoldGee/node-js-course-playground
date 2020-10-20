ec29f7bb183f930834910d8c069649e3a0bccb29
Arnau <agomez1f@gmail.com>
2020-10-20 15:19:33 +0200

    :tada: Started node js playground

---
+++ example.js
@@ -0,0 +1,7 @@
+/**********************************
+ * Using the rest operator
+ **********************************/
+const toArray = (...args) => args;
+console.log(toArray(1,2,3,4,5,6,7,8,9,0));
+
+
